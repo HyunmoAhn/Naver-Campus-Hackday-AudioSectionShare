1320341030c0aed6f597153a33ffe3d67a5b079b
LEE HYEONHO <play_love@likelion.org>
2017-11-23 14:41:52 +0000

    file 이름 오타 수정 route api

--- server/app/controllers/audio_controller.rb
+++ server/app/controllers/audio_controller.rb
@@ -1,4 +1,11 @@
 class AudioController < ApplicationController
     def info
+       @name=params[:id]
+       
+       @data = {
+           id: @name
+       }
+       
+       render :json => @data
     end
 end
