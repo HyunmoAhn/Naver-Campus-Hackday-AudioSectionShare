152e5faae11b8db5000ababbb5e221e68c104b9e
ahnhyunmo <gusahdks@gmail.com>
2017-11-24 04:40:07 +0900

    root로 접근시 ‘audio’ 라우팅으로 이동하도록 변경

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -4,6 +4,9 @@ import Audio from './Audio';
 const routes = (store) => ({
 	path: '/',
 	component: RootLayout,
+	indexRoute: {
+		onEnter: (nextState, replace) => replace('/audio'),
+	},
 	childRoutes: [
 		Audio(store),
 	],
